d3d2eef8cbc352b2979f635b653a93d7f9a03935
Batuhan18 <54512066+Batuhan18@users.noreply.github.com>
2025-04-05 15:52:39 +0300

    ...

---
+++ FoodMartMongoDB/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,59 @@
+﻿using FoodMartMongoDB.Dtos.CategoryDtos;
+using FoodMartMongoDB.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FoodMartMongoDB.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var value = await _categoryService.GetAllCategoryAsync();
+            return View(value);
+        }
+
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            await _categoryService.CreateCategoryAsync(createCategoryDto);
+            return RedirectToAction("Index", "Category");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateCategory(string id)
+        {
+            var value = await _categoryService.GetByIdCategoryAsync(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        {
+            await _categoryService.UpdateCategoryAsync(updateCategoryDto);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> DeleteCategory(string id)
+        {
+            await _categoryService.DeleteCategoryAsync(id);
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ FoodMartMongoDB/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using FoodMartMongoDB.Dtos.ProductDtos;
+using FoodMartMongoDB.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FoodMartMongoDB.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var value = await _productService.GetAllProductsAsync();
+            return View(value);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(CreateProductDto createProductDto)
+        {
+            await _productService.CreateProductAsync(createProductDto);
+            return RedirectToAction("Index");
+        }
+    }
+}
